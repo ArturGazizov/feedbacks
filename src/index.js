01a8175a73448390843baf88acc4297dc7ea8783
Default Profile <znatokly@gmail.com>
2020-06-05 14:05:42 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,159 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+
+const Statistic = ({what,number}) => {
+  return (
+    <React.Fragment>
+    <td>{what}</td>
+    <td>{number}</td>
+    </React.Fragment>
+  )
+}
+
+
+
+
+class Statistics extends React.Component {
+
+	 constructor(props) {
+    super(props)
+
+    
+  }
+
+   makepositiivista=(someobject)=>
+  {
+let result=
+  	100*
+someobject[Object.keys(someobject)[2]]*(1)/
+(someobject[Object.keys(someobject)[0]]+someobject[Object.keys(someobject)[1]]+someobject[Object.keys(someobject)[2]])
+//cannot just return expression
+
+if (isNaN(result))
+	result=0
+  	return result
+  }
+
+  makeaverage=(someobject)=>
+  {
+
+  	let result=(someobject[Object.keys(someobject)[0]]*(-1)+
+someobject[Object.keys(someobject)[2]]*(1))/
+(someobject[Object.keys(someobject)[0]]+someobject[Object.keys(someobject)[1]]+someobject[Object.keys(someobject)[2]])
+//cannot just return expression
+if (isNaN(result))
+	result=0
+  	return result
+  }
+
+
+
+  render() {
+
+if (Object.keys(this.props.thestate).map((it)=>this.props.thestate[it]).reduce((total,add)=>total+add)>0)
+
+    return (
+      <div>
+<table>
+{
+	Object.keys(this.props.thestate).map((it)=>{return <tr><Statistic what={it} number={String(this.props.thestate[it])}/></tr>})
+}
+
+
+<tr><Statistic what={'keskiarvo '} number={String(this.makeaverage(this.props.thestate))}/></tr>
+<tr><Statistic what={'positiivista '} number={String(this.makepositiivista(this.props.thestate))}/></tr>
+
+
+</table>
+
+      </div>
+    )
+
+return(<div></div>)
+
+  }
+}
+
+
+
+const Feedback = ({whattodo,what,howmany}) => {
+  if (whattodo)
+  return (
+    <button onClick={whattodo}>
+      {what}
+    </button>
+  )
+  return (
+    <p>
+      {what}{' '}{howmany}
+    </p>
+  )
+}
+
+const Annapalautetta = () => {
+  return (
+    <div>
+      <p>Give feedback</p>
+    </div>
+  )
+}
+
+const Tilasto = () => {
+  return (
+    <div>
+      <p>Statistics</p>
+    </div>
+  )
+}
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      huono: 0,
+      neutraali: 0,
+      hyvä: 0
+    }
+  }
+
+
+  lisaaArvoon = (whattoraise) => {
+  const renewer=new Object()
+  renewer[whattoraise]=this.state[whattoraise]+1
+  return () => {
+    this.setState(renewer)
+  }
+	}
+
+
+ 
+
+  render() {
+    return (
+
+      <div>
+<Annapalautetta />
+{Object.keys(this.state).map((it)=>{return <Feedback whattodo={this.lisaaArvoon(it).bind(this)} what={it}/>})}
+<Tilasto/>
+
+
+
+<Statistics thestate={this.state}/>
+
+      </div>
+
+
+
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
+
