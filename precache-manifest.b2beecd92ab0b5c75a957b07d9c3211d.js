9c09d97a7f8c2460d939914b5b1b3a6c3419d62a
Default Profile <znatokly@gmail.com>
2020-06-05 14:09:15 +0300

    Updates

---
+++ precache-manifest.b2beecd92ab0b5c75a957b07d9c3211d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f1a7b8cdd9d42e865f7d67bfdc862a0",
+    "url": "/feedbacks/index.html"
+  },
+  {
+    "revision": "1801d9bb3ca03dd48ed9",
+    "url": "/feedbacks/static/js/2.37be1a28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/feedbacks/static/js/2.37be1a28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326cdb468b750cf0e145",
+    "url": "/feedbacks/static/js/main.2ca4365e.chunk.js"
+  },
+  {
+    "revision": "bcd7eb36284a5ae9f9f9",
+    "url": "/feedbacks/static/js/runtime-main.16578b9c.js"
+  }
+]);
